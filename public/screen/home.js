af0cd39eb4986e14b89be7e9f689010d5c25e907
Karan Juneja <abhijeetr.23@gmail.com>
2020-11-10 12:04:04 +0530

    screen styling css

---
+++ public/screen/home.js
@@ -0,0 +1,54 @@
+
+import React, { Component } from 'react';
+import './landing.css';
+
+
+import Navbar from '../components/Navbar';
+import Intro from '../components/intro';
+import Services from '../components/services';
+import Aboutus from '../components/aboutus';
+
+
+
+class home extends Component{
+
+render(){
+
+    
+    return(
+      <div id='wholepage'>
+<div className='home-page'>
+<Navbar/>
+
+ <Intro/>
+
+
+
+<img src='/images/4_1.png' height='550' width='100%' />
+
+
+
+ <Services/> 
+
+ <img src='/images/11.png' height='900' width='100%'/>
+
+
+ <Aboutus/>
+ {/*
+
+<div className="intro2">
+<img src='11.png' height='900' width='80%' /></div>
+<div className="intro3">
+<img src='10_1.png' height="150" widh="50%"/>
+</div>
+
+<div className="intro2">
+<img src='12.png' height='900' width='70%' />
+</div>  */}
+  </div>      
+  </div>
+    )
+}
+
+}
+export default home;
